2127227e601faa5f8872242b7e7ed89cc7458c0a
Diana Williams Oshun <145607859+d-oshun@users.noreply.github.com>
2023-09-22 19:24:45 +0100

    Add files via upload

---
+++ Java-Exercises/Basics/JLab11.java
@@ -0,0 +1,21 @@
+public class JLab11 
+
+// Student Name : Diana Williams Oshun
+// Student Id Number : C00283112
+// Date : 1/9/2022
+
+{
+    public static void main(String args[])
+    {
+        double cm;
+        double inch;
+
+        System.out.print("Enter the value in inches here-> ");
+        inch = EasyIn.getDouble();
+
+        cm = inch * 2.54;
+
+        System.out.print("This is the value in cemtermeters -> " + cm );
+
+    }
+}
