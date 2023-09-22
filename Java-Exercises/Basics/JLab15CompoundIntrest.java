7228f3f38a506d8cd352b3830988703f70f8ea49
Diana Williams Oshun <145607859+d-oshun@users.noreply.github.com>
2023-09-22 19:15:31 +0100

    Add files via upload

---
+++ Java-Exercises/Basics/JLab15CompoundIntrest.java
@@ -0,0 +1,27 @@
+class JLab15CompoundIntrest
+
+// Student Name : Diana Williams Oshun
+// Student Id Number : C00283112
+// Date : 1/9/2022
+
+{
+   public static void main(String args[])
+   {
+        double loanAmount;
+        double intrestRate;
+        double yearsLoan;
+        double repaid;
+        double rateOfIntrest;
+        
+        System.out.print("You have taken out a loan, please enter the loan amout ->");
+        loanAmount = EasyIn.getDouble();
+        System.out.print("What is the intrest rate per annum on your loan -> ");
+        intrestRate = EasyIn.getDouble();
+        System.out.print("Finally enter how many years you have taken out the loan for ->");
+        yearsLoan = EasyIn.getDouble();
+
+        rateOfIntrest = (intrestRate / 100) + 1;
+        System.out.println(Math.pow(rateOfIntrest,yearsLoan)* loanAmount);
+
+   }
+}
