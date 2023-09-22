dbbf60c652b340c505034fbb41a6ba41dc3d02d8
Diana Williams Oshun <145607859+d-oshun@users.noreply.github.com>
2023-09-22 21:05:22 +0100

    Add files via upload

---
+++ Java-Exercises/If Statements/Jlab2b5If.java
@@ -0,0 +1,82 @@
+
+class Jlab2b5If
+
+// Student Name : 		Diana Williams Oshun
+// Student Id Number : 	C00283112
+//using if statements input the number of a month and outputting the name of that month
+{
+
+    public static void main(String[] args) 
+    {
+        final String number1 = "January";
+        final String number2 = "Febuary";
+        final String number3 = "March";
+        final String number4 = "April";
+        final String number5 = "May";
+        final String number6 = "June";
+        final String number7 = "July";
+        final String number8 = "August";
+        final String number9 = "September";
+        final String number10 = "October";
+        final String number11 = "November";
+        final String number12 = "December";
+        int numberEntered;
+
+        System.out.print("Please enter the number of the month of your choosing -> ");
+        numberEntered = EasyIn.getInt();
+
+        if (numberEntered==1)
+            {
+                System.out.print("The month you have chosen is " + number1);
+            }
+        else if (numberEntered==2)
+            {
+                System.out.print("The month you have chosen is " + number2);
+            }
+        else if (numberEntered==3)
+            {
+                System.out.print("The month you have chosen is " + number3);
+            }
+        else if (numberEntered==4)
+            {
+                System.out.print("The month you have chosen is " + number4);
+            }
+        else if (numberEntered==5)
+            {
+                System.out.print("The month you have chosen is " + number5);
+            }
+        else if (numberEntered==6)
+            {
+                System.out.print("The month you have chosen is " + number6);
+            }
+        else if (numberEntered==7)
+            {
+                System.out.print("The month you have chosen is " + number7);
+            }
+        else if (numberEntered==8)
+            {
+                System.out.print("The month you have chosen is " + number8);
+            }
+        else if (numberEntered==9)
+            {
+                System.out.print("The month you have chosen is " + number9);
+            }
+        else if (numberEntered==10)
+            {
+                System.out.print("The month you have chosen is " + number10);
+            }
+        else if (numberEntered==11)
+            {
+                System.out.print("The month you have chosen is " + number11);
+            }
+        else if (numberEntered==12)
+            {
+                System.out.print("The month you have chosen is " + number12);
+            }
+
+        
+    }
+}
+
+
+
